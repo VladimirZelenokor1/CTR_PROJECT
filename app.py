d44d1ef421058eb837452ec0be85f559ffe63cdb
Vladimir Zelenokor <v.zelenokor@innopolis.university>
2024-09-21 21:08:57 +0300

    STARTED INFERENCE

---
+++ app.py
@@ -0,0 +1,107 @@
+import sys
+import logging
+import os
+from typing import List
+import pandas as pd
+import uvicorn
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+import joblib
+from catboost import CatBoostClassifier
+
+from ctr_project.entities.train_pipeline_params import (
+    TrainingPipelineParams,
+    read_training_pipeline_params,
+)
+
+from ctr_project.features import CtrTransformer
+from ctr_project.modeling.model_fit_predict import predict_model
+from tests.features.test_ctr_transformer import ctr_transformer
+
+logger = logging.getLogger(__name__)
+handler = logging.StreamHandler(sys.stdout)
+logger.setLevel(logging.INFO)
+logger.addHandler(handler)
+
+class AdOpportunity(BaseModel):
+    data: list
+    features: list
+
+class ClickResponse(BaseModel):
+    device_ip: str
+    click_proba: float
+
+app = FastAPI()
+
+@app.get('/')
+def main():
+    return "Server started!"
+
+def load_models(training_pipeline_params: TrainingPipelineParams):
+    model = joblib.load(training_pipeline_params.output_model_path)
+    ctr_transformer = joblib.load(training_pipeline_params.output_ctr_transformer_path)
+    return model, ctr_transformer
+
+@app.get('/health')
+def check_models(training_pipeline_params: TrainingPipelineParams):
+    model, ctr_transformer = load_models(training_pipeline_params)
+    if model is None or ctr_transformer is None:
+        logger.error("app/check_models models are None!")
+        raise HTTPException(status_code=500, detail="Failed to load models")
+
+@app.get('/check_schema')
+def check_schema(features: list, training_pipeline_params: TrainingPipelineParams):
+    if not set(training_pipeline_params.feature_params.ctr_features).issubset(
+            set(features)
+    ):
+        logger.error("app/check_schema missing columns")
+        raise HTTPException(
+            status_code=400, detail="Provided features do not match the training pipeline schema"
+        )
+
+def make_predict(
+    data: list,
+    features: list,
+    model: CatBoostClassifier,
+    ctr_transformer: CtrTransformer,
+    training_pipeline_params: TrainingPipelineParams,
+) -> List[ClickResponse]:
+    check_schema(features, training_pipeline_params)
+
+    df = pd.DataFrame(data, columns=features)
+
+    features = ctr_transformer.transform(df)
+    predicted_proba, _ = predict_model(model, features)
+
+    logger.debug("df.device_ip: ", df["device_ip"].values[0])
+    logger.debug("predicted_proba", predicted_proba, predicted_proba[0, 1])
+
+    return [
+        ClickResponse(
+            device_ip=df["device_ip"].values[0],
+            click_proba=round(predicted_proba[0, 1], 4),
+        )
+    ]
+
+@app.post('/predict/', response_model=List[ClickResponse])
+def predict(request: AdOpportunity):
+    logger.debug("app/predict run")
+
+    config_path = "D:/Projects/CTR_PROJECT/configs/train.yaml"
+    training_pipeline_params: TrainingPipelineParams = read_training_pipeline_params(
+        config_path
+    )
+
+    logger.debug(f"app/predict training_pipeline_params: {training_pipeline_params}")
+
+    check_models(training_pipeline_params)
+    logger.debug("app/predict check_models passed")
+
+    model, ctr_transformer = load_models(training_pipeline_params)
+
+    return make_predict(
+        request.data, request.features, model, ctr_transformer, training_pipeline_params
+    )
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="0.0.0.0", port=os.getenv("PORT", 8000))
